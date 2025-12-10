3f6eb2887523bb5e7682d875d2ff5f94b14a1a0f
Ronaldo544 <Imankaashmitha@gmail.com>
2025-12-10 19:17:03 +0700

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,25 @@
+name="Imanka"
+age= 11
+student= True
+weight=36.11
+print("name ", name)
+print("data type is",type(name))
+print("age ", age)
+print("data type is",type(age))
+print("student ", student)
+print("data type is",type(student))
+print("weight ", weight)
+print("data type is",type(weight))
+age=str(age)
+print("data type is",type(age))
+weight=int(weight)
+print("data type is",type(weight))
+
+num1=6
+num2=7
+print("num1 ",num1)
+print("num2 ",num2)
+print("addition ",num1+num2)
+print("subtraction ",num1-num2)
+print("multipication ",num1*num2)
+result=
