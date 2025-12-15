6d81e78d9d1a50064fecd97b74881303c4bd445f
Ronaldo544 <Imankaashmitha@gmail.com>
2025-12-15 19:04:23 +0700

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,11 @@
+import turtle 
+turtle.Screen().bgcolor("Cyan")
+
+cr=turtle.Screen()
+cr.setup(400,300)
+
+turtle.title("Ronaldo is the GOAT")
+
+
+turtle.done()
+
---
+++ turtle2.py
@@ -0,0 +1,36 @@
+import turtle
+
+turtle.Screen(). bgcolor("yellow")
+
+board=turtle.Turtle()
+
+# # Triangle
+# board.forward (100)
+
+# board.left(120)
+# board.forward(100)
+
+# board.left(120)
+# board.left(90)
+
+board.forward(100)
+board.left(90)
+
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+
+turtle.done()
+
+
+
+
+
