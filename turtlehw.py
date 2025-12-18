786b69873675182c95e411e6e3b67f36db743776
Ronaldo544 <Imankaashmitha@gmail.com>
2025-12-18 20:06:05 +0700

    Add files via upload

---
+++ turtlehw.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.Screen(). bgcolor("purple")
+
+board=turtle.Turtle()
+
+# # Triangle
+ board.forward (125)
+
+# board.left(120)
+ board.forward(67)
+
+# board.left(120)
+ board.right(90)
+ 
+board.forward(100)
+board.left(120)
+
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.backward(90)
+
+turtle.done()
