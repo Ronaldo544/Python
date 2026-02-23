183ac62a4970c1fc8efeede777abc06eda18a011
Ronaldo544 <Imankaashmitha@gmail.com>
2026-02-23 17:23:27 +0700

    Add files via upload

--- act1.py
+++ act1.py
@@ -1,12 +1,63 @@
-file= open('Codingal.txt', 'r',encoding='utf-8')
-print(file.read())
-file.close()
+import random
 
-file = open('Codingal.txt', 'r',encoding='utf-8')
-print("\n Read in parts \n")
-print(file.read(8))
-file.close()
+attempts_list = []
 
-file = open('Codingal.txt', 'a',encoding='utf-8')
-file.write("Hi! I am Penguin and I am 1 yr old.")
-file.close()
+def show_score():
+    if len(attempts_list) <= 0:
+        print("There is currently no high score, it's yours for the taking!")
+    else:
+        print("The current high score is {} attempts".format(min(attempts_list)))
+
+def start_game():
+    random_number = random.randint(1, 10)
+
+    print("Hey There! Welcome to the game of guesses!")
+    player_name = input("Enter your name! ")
+    wanna_play = input(
+        "Hi, {}, would you like to play the guessing game? (Enter Yes/No) "
+        .format(player_name)
+    )
+
+    attempts = 0
+    show_score()
+
+    while wanna_play.lower() == "yes":
+        try:
+            guess = input("Pick a number between 1 and 10 ")
+
+            if int(guess) < 1 or int(guess) > 10:
+                raise ValueError("Please guess a number within the given range")
+
+            if int(guess) == random_number:
+                print("Congrats! You guessed it right!")
+                attempts += 1
+                attempts_list.append(attempts)
+
+                print("It took you {} attempts".format(attempts))
+                play_again = input("Would you like to play again? (Enter Yes/No) ")
+
+                attempts = 0
+                show_score()
+                random_number = random.randint(1, 10)
+
+                if play_again.lower() == "no":
+                    print("That's cool, have a nice day!")
+                    break
+
+            elif int(guess) < random_number:
+                print("It's higher")
+                attempts += 1
+
+            elif int(guess) > random_number:
+                print("It's lower")
+                attempts += 1
+
+        except ValueError as err:
+            print("Oh! That is not a valid value. Try again...")
+            print("({})".format(err))
+
+    else:
+        print("That's cool, have a nice day!")
+
+if __name__ == "__main__":
+    start_game()
