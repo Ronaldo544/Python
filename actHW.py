5a67dcfa55603f0c427cfb1386fd9cd2c1023e10
Ronaldo544 <Imankaashmitha@gmail.com>
2026-02-24 16:39:48 +0700

    Add files via upload

---
+++ actHW.py
@@ -0,0 +1,12 @@
+file = open("RGOAT.txt", "w")
+
+file.write("Why Cristiano Ronaldo is the GOAT\n"
+           "Most goals in football history\n"
+           "Dominated England, Spain, Italy\n"
+           "Clutch in big games\n"
+           "Elite mentality\n"
+           "20+ years at the top\n"
+           "No excuses. No limits.\n"
+           "The GOAT.")
+
+file.close()
