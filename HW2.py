0f1dd56d28705707f8c7a203747a7d4ba4b5f4d0
Ronaldo544 <Imankaashmitha@gmail.com>
2026-03-02 21:07:47 +0700

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,135 @@
+score = 0
+
+print("Ultimate Footabll Quiz🔥⚽")
+print("Type a,b,c or d \n")
+
+print ("1) Who us the GOAT of Football?")
+print("a) Messi")
+print("b) Neymar")
+print("c) Ronaldo")
+print("d) Mbappe")
+
+answer = input("Your answer:").lower()
+if answer == "c":
+    print("Correct 🐐 You know who the real GOAT is. SIUUUUU \n ")
+    score += 1
+else:
+    print("Wrong💀 L football knowledge")
+
+print("2)Where was the 2014 FIFA World Cup hosted?")
+print("a) Germany")
+print("b) Brazil")
+print("c) South Africa")
+print("d) Russia")
+
+answer = input("Your answer:").lower()
+if answer == "b":
+    print("Correct BRAZILLL \n")
+    score += 1
+else:
+    print("Nah Footaball history failed 💀⚽🗺 \n")
+
+print("3)Which club has won the MOST Champions League titles?")
+print("a) Real Madrid")
+print("b) Barcelona")
+print("c) AC Milan")
+print("d) Bayern Munich")
+
+answer = input("Your answer:").lower()
+if answer == "a":
+    print("Correct UCL royalty \n")
+    score += 1
+else:
+    print("Wrong 😭😱 where did u get that from Tik tok ? \n")
+
+
+print("4)How long is a standard football match?")
+print("a) 80 minutes")
+print("b) 100 minutes")
+print("c) 60 mintues")
+print("d) 90 minutes")
+
+answer = input("Your answer:").lower()
+if answer == "d":
+    print("Correct. You know the basics :) \n")
+    score += 1
+else:
+    print("Wrong 😭 do even watch football? \n")
+
+
+print("5)Which country won the EURO 2016?")
+print("a) France")
+print("b) Portugal")
+print("c) Spain")
+print("d) Italy")
+
+answer = input("Your answer:").lower()
+if answer == "b":
+    print("Correct The GOATED country \n")
+    score += 1
+else:
+    print("Wrong  The coached the team even on the bench 😲 \n")
+
+
+print("6)Which position does Ronaldo mainly play?")
+print("a) Forward/Striker")
+print("b) Goalkeeper")
+print("c) Defender")
+print("d) Midfielder")
+
+answer = input("Your answer:").lower()
+if answer == "a":
+    print("Correct Obviously W \n")
+    score += 1
+else:
+    print("Wrong 💀 Unistall football im sorry its not meant for you \n")
+
+
+print("7)What color means a player is sent off?")
+print("a) Blue")
+print("b) Green")
+print("c) Yellow")
+print("d) Red")
+
+answer = input("Your answer:").lower()
+if answer == "d":
+    print("Correct Get this 🟥 and your cooked RIP \n")
+    score += 1
+else:
+    print("Wrong 😭 Ref pulling out a red no two reds for that.💀 ? \n")
+
+    print("🎯🎉Quiz Over")
+    print("Score:",score,"/7")
+
+    if score == 7:
+     print("Elite Ball Knowledge and IQ 🐐 True Fan👏")
+
+    elif score >=5:
+        print("Respectable Strong Ball knowledge 🎉")
+    
+    elif score >= 3:
+        print("Mid knowledge should watch more youtube short or tit tok reels. 🙏🤐")
+
+    else:
+        print("Relegation Mode 💀. That deserves a RED card no 3 red cards 🟥🟥🟥")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
